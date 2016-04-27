5a7be669a4eebebd2bb636eeb7e4e36afe30cedd
fletcher <fletcherupchurch@gmail.com>
2016-04-27 13:18:21 -0400

    css styling

---
+++ app/models/tweet.rb
@@ -0,0 +1,8 @@
+class Tweet < ActiveRecord::Base
+
+  validates :message, presence: true, length: {maximum: 140,
+  too_long: "A tweet is only 140 max. Everybody knows that!"}
+
+  belongs_to :user
+  has_many :likes
+end
